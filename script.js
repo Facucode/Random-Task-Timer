e546a3aef556b2ead0a97d54b1004054b317c60e
agustinpfs <agustinpfs@gmail.com>
2018-09-28 08:56:59 -0300

    add screen 2

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 
-    function colon(x){
-        let y=0;
-        var regex=/^(2[0-3])|[01][0-9]$/;
-        if(regex.test(x.value) && y===0){
-            y++;
-           x.value+=":";
-           
-        }
-     }
+function colon(x){
+    let y=0;
+    var regex=/^(2[0-3])|[01][0-9]$/;
+    if(regex.test(x.value) && y===0){
+        y++;
+        x.value+=":";
+        
+    }
+}
     
 
-    let timer=document.querySelector(".header h2");
+let timer=document.querySelector(".header h2");
 
 
-    let buttons=document.querySelectorAll("button");
-        let regex = /(\d+)/g;
-        for(let i=0;i<4;i++){
-             buttons[i].addEventListener('click',function(){
-                let time = `0${buttons[i].innerHTML.match(regex)}:00`;
-                timer.innerHTML=time;
-            })
-   
-            
+let buttons=document.querySelectorAll("button");
+let regex = /(\d+)/g;
+
+for(let i=0;i<4;i++){
+        buttons[i].addEventListener('click',function(){
+        let time = `0${buttons[i].innerHTML.match(regex)}:00`;
+        timer.innerHTML=time;
+    })           
 }
 
-    let customize=document.querySelector(".customize");
-    customize.addEventListener('click',function(){
-    document.querySelector(".header").innerHTML="<h1>¿Cuanto tiempo vas a trabajar hoy?</h1><input type=\"text\" onkeyup=\"colon(this)\" maxlength=\"5\">";
+let customize=document.querySelector(".customize");
+customize.addEventListener('click',function(){
+    document.querySelector(".sreen1__header").innerHTML="<h1>¿Cuanto tiempo vas a trabajar hoy?</h1><input type=\"text\" onkeyup=\"colon(this)\" maxlength=\"5\">";
     document.querySelector("input").focus();
-    })
+})
 
-   
+let next = document.querySelector(".next__h2");
+next.addEventListener('click',function(){
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s2").style.display = "block";
+})
+
+
 
 
 
